0917fb1e61c6f670cd60b5d546d04e86b9065164
lisalevinson <lisalev@umich.edu>
2023-11-18 10:37:23 -0500

    major update
    major redesign of main.js, putting practice trials in template, figuring out better button layout and implementing feedback for practice. new instructions file.

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,7 +1,7 @@
 PennController.ResetPrefix(null);
 DebugOff();
 
-var shuffleSequence = seq( "consent", "IDentry", "demo", "intro",
+var shuffleSequence = seq( "consent", "IDentry", "demo", "instructions",
                             "startpractice",
                                             
                             followEachWith("sep",seq("practice")),
@@ -11,7 +11,8 @@ var shuffleSequence = seq( "consent", "IDentry", "demo", "intro",
 
                             followEachWith("sep",seq(rshuffle(
                                 startsWith("mklo"),
-                                startsWith("gp")
+                                startsWith("gp"),
+                                startsWith("misc")
                             ))),
 
                             "sendresults",
@@ -21,36 +22,10 @@ var shuffleSequence = seq( "consent", "IDentry", "demo", "intro",
 
 var showProgressBar = false;
 
-
-Template("Experiment.csv", row => {
-   items.push(
-    [[row.label, row.item], "PennController", newTrial(
-        newText("Reminder: Press f for yes, j for no")
-                .print("center at 50vw", "middle at 30vh")
-            ,
-        newController("StopMakingSense", {
-            s: row.sentence,
-            yesKeyCode: "70", 
-            noKeyCode:"74",
-            smsIndex: row.sms != 0 ? row.sms - 1 : null
-        })
-            .print("center at 50vw", "middle at auto")
-            .log()
-            .wait()
-    )
-    .log("counter", __counter_value_from_server__)
-    .log("label", row.cond1)
-    .log("latinitem", row.item)
-    ] 
-   );
-   return newTrial('_dummy_',null);
-})
-
 Header(
    newVar("partID").global()   
 )
 .log( "partid" , getVar("partID") )
-
 
 newTrial("IDentry",
    newText("instr", "Please enter your Prolific ID:").print()
@@ -60,54 +35,6 @@ newTrial("IDentry",
    newButton("Next").print().wait(
        getVar("partID").set( v=>$("#partID").val() ).testNot.is('')
    )
-)
-
-newTrial("practice",
-        newText("Reminder: Press f for yes, j for no")
-                .print("center at 50vw", "middle at 30vh")
-            ,
-        newController("StopMakingSense", {
-            s: "The carpenter ordered food from the restaurant",
-            yesKeyCode: "70", 
-            noKeyCode:"74",
-            // - 1 since .csv is not 0 indexed 
-            smsIndex: null
-        })
-            .print("center at 50vw", "middle at auto")
-            .log()
-            .wait()
-)
-
-newTrial("practice",
-        newText("Reminder: Press f for yes, j for no")
-                .print("center at 50vw", "middle at 30vh")
-            ,
-        newController("StopMakingSense", {
-            s: "The cat has the played",
-            yesKeyCode: "70", 
-            noKeyCode:"74",
-            // - 1 since .csv is not 0 indexed 
-            smsIndex: 4
-        })
-            .print("center at 50vw", "middle at auto")
-            .log()
-            .wait()
-)
-
-newTrial("practice",
-        newText("Reminder: Press f for yes, j for no")
-                .print("center at 50vw", "middle at 30vh")
-            ,
-        newController("StopMakingSense", {
-            s: "A tired man the for hours",
-            yesKeyCode: "70", 
-            noKeyCode:"74",
-            // - 1 since .csv is not 0 indexed 
-            smsIndex: 3
-        })
-            .print("center at 50vw", "middle at auto")
-            .log()
-            .wait()
 )
 
 newTrial("demo",
@@ -129,27 +56,86 @@ newTrial("demo",
            )
 )
 
+Template("practice.csv", row =>
+    newTrial("practice",
+            newText("Does the last word make sense? Press f to start:")
+                .print("center at 50vw", "middle at 8vh")
+                ,
+             newText("keyreminder", 
+                "<html> <head> <style> table { width: 100%; border-collapse: collapse; } th, td { padding: 8px; } th:nth-child(1), td:nth-child(1) { width: 20%; } th:nth-child(2), td:nth-child(2) { width: 60%; } th:nth-child(3), td:nth-child(3) { width: 20%; } </style> </head> <body> <table> <tr> <th>YES</th> <th> </th> <th>NO</th> </tr> <tr> <td>f</td> <td> </td> <td>j</td> </tr> </table> </body> </html>")
+                .css("font-family", "Helvetica, sans-serif")
+                .css("font-size", "16px")
+                .print("center at 50vw", "middle at 40vh"),
+            newController("StopMakingSense", {
+                s: row.sentence,
+                yesKeyCode: "70", 
+                noKeyCode:"74",
+                // - 1 since .csv is not 0 indexed 
+                smsIndex: null
+            })
+                .print("center at 50vw", "middle at 20vh")
+                .log()
+                .wait()
+                ,
+            getText("keyreminder")
+                .text(row.feedback)
+              //  .wait()
+              ,
+            newButton("continue","continue")
+                .print("center at 50vw", "middle at 50vh")
+                .wait()
+    )
+)
+
+Template("experiment.csv", row => {
+   items.push(
+    [[row.label, row.item], "PennController", newTrial(
+        newText("Press f to start:")
+            .print("center at 50vw", "middle at 8vh")
+            ,        
+        newText("Reminder: Press f for yes, j for no")
+                .print("center at 50vw", "middle at 40vh")
+            ,
+        newController("StopMakingSense", {
+            s: row.sentence,
+            yesKeyCode: "70",   // 70 = f
+            noKeyCode:"74",     // 74 = j
+            smsIndex: row.sms != 0 ? row.sms - 1 : null
+        })
+            .print("center at 50vw", "middle at 20vh")
+            .log()
+            .wait()
+    )
+    .log("counter", __counter_value_from_server__)
+    .log("label", row.cond1)
+    .log("latinitem", row.item)
+    ] 
+   );
+   return newTrial('_dummy_',null);
+})
+
+// this is where native ibex items are defined
 var items = [
 
     ["sep", "Separator", {transfer: 1500, normalMessage: "Please wait for the next item."}],
 
+// messages
     ["startpractice", Message, {consentRequired: false,
         html: ["div",
                ["p", "First you can do three practice sentences."]
               ]}],
-
-    ["setcounter", "__SetCounter__", { }],
-    ["sendresults", "__SendResults__", { }],
-
-    ["consent", "Form", { html: { include: "consent.html" } } ],
-
     ["starter", Message, {consentRequired: false,
         html: ["div",
                ["p", "Time to start the main portion of the experiment!"]
               ]}],
-             
-      
-     ["completion", "Form", {continueMessage: null, html: { include: "completion.html" } }]
-    
+
+// html pages
+    ["consent", "Form", { html: { include: "consent.html" } } ],
+    ["instructions", "Form", { html: { include: "instructions.html" } } ],
+    ["completion", "Form", {continueMessage: null, html: { include: "completion.html" } }],
+
+    ["setcounter", "__SetCounter__", { }],
+    ["sendresults", "__SendResults__", { }]
+
 ]
 
